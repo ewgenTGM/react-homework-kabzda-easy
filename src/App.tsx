1cf5047979675de8b24d44d47211b66d980ecbfc
ewgenTGM <ewgenbi@gmail.com>
2021-02-07 18:04:00 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import OnOff from "./components/OnOff";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type propsType = {
+    title: string
+}
+
+function App(props: propsType) {
+    return (
+        <div className="App">
+            <div className={styles.app_title}>
+                {props.title}
+            </div>
+            <OnOff on={true}/>
+            <OnOff on={false}/>
+        </div>
+    );
 }
 
 export default App;
